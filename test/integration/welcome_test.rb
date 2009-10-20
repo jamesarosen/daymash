837cbde460a2dfce7c96a9aae4d189c09d9b6d51
James A. Rosen <james.a.rosen@gmail.com>
2009-10-20 16:10:43 -0700

    added 'Sign Up' and 'Sign In' links to navbar; added tests that links exist

--- test/integration/welcome_test.rb
+++ test/integration/welcome_test.rb
@@ -14,6 +14,14 @@ class WelcomeTest < ActionController::IntegrationTest
       assert_select 'body', /\b(welcome|h[eu]llo|hi|wilkommen)\b/i
     end
     
+    should 'allow users to sign up' do
+      assert_select 'a', /sign up/i
+    end
+    
+    should 'allow users to sign in' do
+      assert_select 'a', /sign in/i
+    end
+    
     should_take_on_average_less_than 0.3.seconds do
       get '/'
     end
