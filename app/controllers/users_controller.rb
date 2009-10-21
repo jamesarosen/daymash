b3e1c035043dfe06e31b7fb559546a6c3235f36d
James A. Rosen <james.a.rosen@gmail.com>
2009-10-21 12:36:12 -0700

    refactored UsersController: moved sign-in call from helper method to inline in :create since it doesn't get re-used elsewhere and is only one line

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,17 +7,11 @@ class UsersController < ApplicationController
   def create
     @user ||= User.new(params[:user])
     if @user.save
-      sign_in(@user)
+      UserSession.create(@user)
       redirect_to root_path
     else
       render :action => 'new'
     end
-  end
-  
-  private
-  
-  def sign_in(user)
-    UserSession.create(user)
   end
   
 end
