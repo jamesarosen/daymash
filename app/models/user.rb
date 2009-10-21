b7397c72702494f3e5371a742fde276c6f50147b
James A. Rosen <james.a.rosen@gmail.com>
2009-10-21 07:27:16 -0700

    User#to_s now uses display_name if present

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ActiveRecord::Base
   acts_as_authentic
+  
+  def to_s
+    display_name.present? ? display_name : username
+  end
 end
--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -12,5 +12,19 @@ class UserTest < ActiveSupport::TestCase
     should_validate_uniqueness_of :username, :email
     should_not_allow_values_for :password, nil, '', 'a', 'ab', 'abc'
   end
+  
+  context 'a User with a display_name' do
+    setup { @user = Factory(:user, :display_name => 'Fred Cartwright') }
+    should 'use the display name for :to_s' do
+      assert_equal 'Fred Cartwright', @user.to_s
+    end
+  end
+  
+  context 'a User without a display_name, but with a username' do
+    setup { @user = Factory(:user, :username => 'fred12', :display_name => nil) }
+    should 'use the display name for :to_s' do
+      assert_equal 'fred12', @user.to_s
+    end
+  end
     
 end
