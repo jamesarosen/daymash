1ffd6f58437c811aabb083fdd1fddb80999c8597
James A. Rosen <james.a.rosen@gmail.com>
2009-10-22 14:59:10 -0700

    changed sign_in and sign_out test macros to work with Webrat (finally)

--- test/shoulda_macros/authentication_macros.rb
+++ test/shoulda_macros/authentication_macros.rb
@@ -1,11 +1,18 @@
 module AuthenticationMacros
   
   def sign_out!
-    delete sign_out_path
+    visit '/'
+    return unless controller.signed_in?
+    click_link 'Sign Out'
   end
   
   def sign_in_as(user, password = 'password')
-    post new_user_session_path :principal => user.username, :password => password
+    visit '/'
+    return if controller.signed_in?
+    click_link 'Sign In'
+    fill_in 'user_session_principal', :with => user.username
+    fill_in 'user_session_password',  :with => password
+    click_button 'user_session_submit'
   end
   
 end
