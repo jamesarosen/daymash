1bb42348776fbe0714100e04e3ce40cd9b9ee970
James A. Rosen <james.a.rosen@gmail.com>
2009-10-22 21:45:28 -0700

    added some validations to Calendar

--- app/models/calendar.rb
+++ app/models/calendar.rb
@@ -1,5 +1,8 @@
 class Calendar < ActiveRecord::Base
   
   belongs_to :user, :touch => true
+  validates_presence_of :uri, :title, :user
+  validates_format_of :uri, :with => /^https?:\/\/.+\..+/
+  validates_uniqueness_of :uri, :title, :scope => :user_id
   
 end
--- test/unit/calendar_test.rb
+++ test/unit/calendar_test.rb
@@ -1,8 +1,18 @@
 require 'test_helper'
 
 class CalendarTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  
+  context 'a Calendar' do
+    subject { @calendar }
+    setup do
+      @user = Factory(:user)
+      @other_calendar = Factory(:calendar)
+      @calendar = Calendar.new
+    end
+    should_validate_presence_of :uri, :title, :message => /.+/
+    should_validate_uniqueness_of :uri, :title, :scoped_to => :user_id
+    should_not_allow_values_for :uri, nil, '', 'a'
+    should_allow_values_for :uri, 'http://foo.bar', 'https://baz.yoo/hoo.ics'
   end
+    
 end
