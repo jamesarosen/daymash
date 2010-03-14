02019d866ea3c3ce625c9e3b54cb66d87380a3cc
James A. Rosen <james.a.rosen@gmail.com>
2010-03-14 19:46:48 -0400

    added Rack::Rpx dependency and API keys example file

--- config/environment.rb
+++ config/environment.rb
@@ -35,6 +35,9 @@ Rails::Initializer.run do |config|
   
   config.gem 'xebec', :source => 'http://rubygems.org',
                       :version => '~> 2.2.0'
+  
+  config.gem 'rack-rpx',  :source => 'http://rubygems.org',
+                          :version => '~> 0.1.5'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
