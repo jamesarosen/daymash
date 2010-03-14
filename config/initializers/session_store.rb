7040c4bb884e5a357c8ea47b4351fc18569946e6
James A. Rosen <james.a.rosen@gmail.com>
2010-03-14 11:16:31 -0400

    gsub/less scheduling/daymash, removed links to Less Everything in the footer

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -5,8 +5,8 @@
 # Make sure the secret is at least 30 characters and all random, 
 # no regular words or you'll be exposed to dictionary attacks.
 ActionController::Base.session = {
-  :key         => '_less_scheduling_session',
-  :secret      => '198e25974beb362b827f3553f354165ae3ccb8a76a1d364c09cd0a5e599514bd887162aaea8a24948485dfc3c6062256e48ef7f2d91860d5c73e949efbe5397f'
+  :key         => '_daymash_session',
+  :secret      => '198e25974beb362b827f3553f77492de3ccb8a76a1dc09cd0a5e599514bdc887162aaea8a24948485dfc3c601039edfe48ef7f2d91860d5c73e9efbe5397f'
 }
 
 # Use the database for sessions instead of the cookie-based default,
