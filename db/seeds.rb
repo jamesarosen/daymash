59801511588b0c97783bad0af28e39d2c86135da
James A. Rosen <james.a.rosen@gmail.com>
2010-03-20 19:10:01 -0400

    added Facebook, Twitter, and Google credentials to my account in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,4 +7,7 @@
 #   Major.create(:name => 'Daley', :city => cities.first)
 
 u = User.create(:email => 'james.a.rosen@gmail.com', :display_name => 'James Rosen')
-Credential.create(:user => u, :provider => 'MyOpenID', :identifier => 'http://jamesarosen.com/')
+Credential.create :user => u, :provider => 'MyOpenID', :identifier => 'http://jamesarosen.com/'
+Credential.create :user => u, :provider => 'Twitter',  :identifier => 'http://twitter.com/account/profile?user_id=7114202'
+Credential.create :user => u, :provider => 'Facebook', :identifier => 'http://www.facebook.com/profile.php?id=3102254'
+Credential.create :user => u, :provider => 'Google',   :identifier => 'https://www.google.com/accounts/o8/id?id=AItOawmS8cYL_iLLHYXSUTCu5gfzDlaqeDHA294'
