f1003a5316974d111ff84b3f8ab3589aa8c3fc79
James A. Rosen <james.a.rosen@gmail.com>
2010-03-22 11:57:36 -0400

    aggregate link now includes privacy token

--- app/helpers/aggregates_helper.rb
+++ app/helpers/aggregates_helper.rb
@@ -1,2 +1,8 @@
 module AggregatesHelper
+  
+  def aggregate_link(user)
+    url = user_aggregate_url :user_id => user.to_param, :format => 'ics', :pt => user.privacy_token
+    link_to url, url
+  end
+  
 end
