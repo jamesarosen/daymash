fb8364e8115c495408496cbb4f75dd1a70726613
James A. Rosen <james.a.rosen@gmail.com>
2010-04-03 19:08:35 -0400

    changed the API keys initializer to load keys into a constant so other parts of the application have access to them

--- config/initializers/api_keys.rb
+++ config/initializers/api_keys.rb
@@ -1,10 +1,16 @@
-config_path = File.join(File.dirname(__FILE__), '..', 'api_keys.yml')
-api_keys = if File.exist?(config_path)
-  YAML.load File.new(config_path)
-else
-  {}
-end.with_indifferent_access
+module DayMash
+  config_path = File.join(File.dirname(__FILE__), '..', 'api_keys.yml')
+  API_KEYS = if File.exist?(config_path)
+    YAML.load File.new(config_path)
+  else
+    {}
+  end.with_indifferent_access
+end
 
 if defined?(:RPXNow)
-  RPXNow.api_key = ENV['RPX_API_KEY'] || api_keys[:rpx]
+  RPXNow.api_key = ENV['RPX_API_KEY'] || DayMash::API_KEYS[:rpx]
 end
+
+amazon_config = (DayMash::API_KEYS['amazon'] || {})['s3'] || {}
+ENV['AMAZON_ACCESS_KEY_ID'] = amazon_config['access_key_id']
+ENV['AMAZON_SECRET_ACCESS_KEY'] = amazon_config['secret_access_key']
