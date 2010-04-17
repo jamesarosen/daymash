f114b44b65b703874439a2e51f95ab7d1831bbca
James A. Rosen <james.a.rosen@gmail.com>
2010-04-17 17:10:51 -0400

    moved Mocha requirement to before Shoulda requirement to fix load order problem

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -10,9 +10,9 @@ config.action_controller.allow_forgery_protection    = false
   
 config.action_mailer.delivery_method = :test
 
+config.gem 'mocha',         :version => '~> 0.9.8',   :source => 'http://rubygems.org/'
 config.gem 'shoulda',       :version => '~> 2.10.3',  :source => 'http://rubygems.org/'
 config.gem 'factory_girl',  :version => '~> 1.2.3',   :source => 'http://rubygems.org/'
-config.gem 'mocha',         :version => '~> 0.9.8',   :source => 'http://rubygems.org/'
 config.gem 'redgreen',      :version => '~> 1.2.2',   :source => 'http://rubygems.org/'
 config.gem 'fakeweb',       :version => '~> 1.2.8',   :source => 'http://rubygems.org/'
 config.gem 'timecop',       :version => '~> 0.2.1',   :source => 'http://rubygems.org/'
